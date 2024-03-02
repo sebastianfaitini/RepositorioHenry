7ef5202b106506ea27da6150f8b58a48df8a7c42
sebastianfaitni <sebastianfaitini@hotmail.com>
2024-03-02 07:43:12 -0300

    blclesFor

---
+++ buclesFor.js
@@ -0,0 +1,20 @@
+var suma=0;
+// // suma = suma +1;
+// // suma = suma +2;
+// // suma = suma +3;
+// // suma = suma +4;
+
+// console.log(suma)
+
+// for (var i = 0; i < 10; i++){
+//     suma = suma +1;
+//     console.log(suma);
+// }
+
+for (let i = 0; i < 5; i++) {
+    suma = suma + i;
+    console.log('Variable de iteración: ', i);
+ }
+ console.log('Variable suma: ', suma);
+
+ 
